eb4fc2c312b85b2b1259141e700e9caaeb55686f
Rodrigo Vianna Oliveira <rodrigo.vianna.oliveira@gmail.com>
2020-09-20 17:19:46 -0300

    Add action pipeline (#2)
    * Add .circleci/config.yml
    
    * adjust to compile
    
    * adjust to compile
    
    * adjust to compile
    
    * adjust to compile
    
    * adjust to compile
    
    * add workflow file
    
    * add workflow file
    
    Co-authored-by: Rodrigo Vianna Calixto de Oliveira <rodrigo.calixto@xpi.com.br>

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,28 +1,6 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-android-extensions")
-    id("kotlin-kapt")
-}
-
 android {
     dataBinding {
         isEnabled = true
-    }
-
-    compileSdkVersion(Versions.compileSdkVersion)
-    buildToolsVersion(Versions.buildToolsVersion)
-
-    defaultConfig {
-        minSdkVersion(Versions.minSdkVersion)
-        targetSdkVersion(Versions.targetSdkVersion)
-        versionCode = Versions.versionCode
-        versionName = Versions.versionName
-
-        testInstrumentationRunner = Depends.runnerPackage
-
-        vectorDrawables.useSupportLibrary = true
-        multiDexEnabled = true
     }
 }
 
